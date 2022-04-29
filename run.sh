7ed0855a5c2cd4fa872e0a17a757fe0760d7b6c3
wrmw <90426896+wrmw@users.noreply.github.com>
2022-04-29 15:38:47 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 set -m
 /usr/bin/Xvfb :1 -screen 0 1180x800x24+32
 x11vnc -display :1 -xkb &
-./noVNC/utils/novnc_proxy --vnc localhost:5901 --listen $PORT &
+./noVNC/utils/novnc_proxy --vnc localhost:5900 --listen $PORT &
 fg %1
